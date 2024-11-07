d58747d2452cc31b74a854b185cf923dfdb14e7d
Ben Wright <bwright@2u.com>
2024-11-07 09:21:01 -0600

    added a fun calculating message

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -27,6 +27,7 @@ export default function Calculator() {
       <button className="btn btn-primary" type="button" onClick={handleAddClick}> Add </button>
       <p className="card-text">Total: {calcState.total}</p>
       <p className="card-text">Previous Total: {calcState.previousTotal}</p>
+      <p>Calculating is fun</p>
     </div>
   );
 }
